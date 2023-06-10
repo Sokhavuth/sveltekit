5b915ddad3205ca108b29ad031920b5f2c209b18
Sokhavuth <vuthdevelop@gmail.com>
2023-06-10 11:39:48 +0700

    Make it better

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Khmerweb Sveltekit</title>
+    <link href="/fonts/setup.css" rel="stylesheet" />
+    <link href="/styles/base.css" rel="stylesheet">
+    <script src="/scripts/jquery.js"></script>
+</svelte:head>
+
+<slot />
